91f205728d9aaede94feaf6ba5adb93ee99ec05e
akhiln8nrepo <akhil.vfn24@gmail.com>
2026-03-22 21:50:58 -0400

    Update craco.config.js

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -3,23 +3,13 @@ const path = require('path');
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      // Force webpack to resolve ajv from the correct location
+
+      // Fix the @ alias to point to src folder
       webpackConfig.resolve = webpackConfig.resolve || {};
-      webpackConfig.resolve.alias = webpackConfig.resolve.alias || {};
-      
-      // Point all ajv imports to the single correct version
-      webpackConfig.resolve.alias['ajv'] = path.resolve(
-        __dirname,
-        'node_modules/ajv'
-      );
-      webpackConfig.resolve.alias['ajv-keywords'] = path.resolve(
-        __dirname,
-        'node_modules/ajv-keywords'
-      );
-      webpackConfig.resolve.alias['schema-utils'] = path.resolve(
-        __dirname,
-        'node_modules/schema-utils'
-      );
+      webpackConfig.resolve.alias = {
+        ...webpackConfig.resolve.alias,
+        '@': path.resolve(__dirname, 'src'),
+      };
 
       return webpackConfig;
     },
